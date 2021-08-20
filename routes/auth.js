bfeb397ffc54e8dcf062a86abf7269b1f2fc2425
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-20 14:34:49 -0300

    login de usuario

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,21 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { crearUsuario } = require('../controllers/auth');
+const { crearUsuario, login } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.post('/new', [
- check('nombre', 'El nombre es obligatorio').not().isEmpty(),
- check('email', 'El correo es obligatorio').isEmail(),
- check('password', 'La contraseña es obligatoria').not().isEmpty(),
- validarCampos
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El correo es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validarCampos
 ], crearUsuario);
+
+router.post('/', [
+    check('email', 'El correo es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validarCampos
+], login);
 module.exports = router;
