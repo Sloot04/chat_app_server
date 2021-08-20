194c125042aa71681d98d452bf35c08885ab234f
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-20 10:42:50 -0300

    Modelo usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,39 @@
-const { response } = require("express");
-const { validationResult } = require("express-validator");
+const { response } = require('express');
+const Usuario = require('../models/usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'Crear usuario!!'
-    });
+    const { email } = req.body;
+
+    try {
+
+        const existeEmail = await Usuario.findOne({ email });
+
+        if (existeEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            usuario
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+
 }
 
 module.exports = {
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,4 @@
 const { io } = require('../index');
-const Band = require('../models/band');
-const Bands = require('../models/bands');
-
-const bands = new Bands();
-
-bands.addBand(new Band('Metallica'));
-bands.addBand(new Band('Queen'));
-bands.addBand(new Band('Pescado rabioso'));
-bands.addBand(new Band('v8'));
 
 
 //Mensajes de Sockets
