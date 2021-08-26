ab7238e5e5e57d0ebb23778290d2f1bce6d3b2d6
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-26 12:17:31 -0300

    Autenticando cliente conectado por socket

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,19 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
 
 
 //Mensajes de Sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado');
+
+    //Cliente con JWT
+   // console.log(client.handshake.headers['x-token']);
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+   if (!valido) {return client.disconnect();}
+
+   console.log('cliente autenticado');
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
